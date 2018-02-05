37eba5ad41106676e9d4ef040e4a6cc44a293a25
papers <paperslove@users.noreply.github.com>
2018-02-05 02:28:17 +0200

    Update basiclog.js
    moved to websockets for reliability and performance reasons

--- basiclog.js
+++ basiclog.js
@@ -1,65 +1,50 @@
-var fs = require('fs');
-const config = require('./config');
+const fs = require('fs');
+const http = require('http');
+const WebSocket = require('ws');
 
-var express = require('express');
-var app = express();
-app.use(express.static('res'));
+const config = require('./config');
 
 const EOL = require('os').EOL;
 var body = '';
+const port = 1489;
+const host = 'localhost';
 
-function getActiveWeapon(obj) {
-    for (let prop in obj)
-        if (obj.hasOwnProperty(prop))
-            if (obj[prop].state != "holstered")
-                return obj[prop];
-}
-
-function teamName(teamcode) {
-    if (teamcode == "T")
-        return "Terrorist";
-    if (teamcode == "CT")
-        return "Counter-Terrorist";
-    else
-        return "unassigned";
-}
-
-var logString = "";
-var oldlog;
-
-app.get('/overlay', (req, res) => {
-    //res.set('Content-Type', 'text/html');
-    res.sendFile(__dirname + '/overlay.html');
+const wss = new WebSocket.Server({ port: 3232 });
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+    setInterval(() => {
+        ws.send(body);
+    }, 15);
 });
-app.get('/', (req, res) => {
-//  res.send('leave this place');
-  res.sendStatus(418);
-});
-var options = {
-    root: __dirname + '/res/',
-    dotfiles: 'deny',
-    headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
+
+server = http.createServer((req, res) => {
+    if (req.method == 'POST') {
+        //console.log("Handling POST request...");
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        body = '';
+        req.on('data', (data) => {
+            body += data;
+        });
+        req.on('end', () => {
+            //console.log("POST payload: " + body);
+            res.end('');
+        });
     }
-};
+    else {
+        console.log("Not expecting other request types...");
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        var html = '<html><body>HTTP Server at http://' + host + ':' + port + '</body></html>';
+        res.end(html);
+    }
 
-var payload;
-var packedload;
-app.post('/animestate', (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    req.on('data', (data) => {
-        payload = JSON.parse(data);
-        packedload = data;
-        // console.log(payload);
-    })
-    res.end(packedload);
 });
-app.listen(80);
 
-console.log('Listening at http://' + config.host + ':' + config.port);
+server.listen(port, host);
+console.log('Listening at http://' + host + ':' + port);
 
-// theres some choke if you have high CPU usage btw
 process.on('uncaughtException', function (err) {
-  console.log(err);
+    console.log(err);
 })
